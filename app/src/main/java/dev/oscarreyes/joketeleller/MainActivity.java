2d59177a2841a5f6f8736f4f15a3ade0dffdd84c
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-04-13 20:24:05 -0500

    * App main activity using displayer module
    * Adapted main app activity to make use of displayer module

--- app/src/main/java/dev/oscarreyes/joketeleller/MainActivity.java
+++ app/src/main/java/dev/oscarreyes/joketeleller/MainActivity.java
@@ -2,13 +2,38 @@ package dev.oscarreyes.joketeleller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import dev.oscarreyes.jokeanswerdisplayer.AnswerActivity;
 
 public class MainActivity extends AppCompatActivity {
+	private TextView textJokeQuery;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		this.loadViews();
+		this.loadData();
+	}
+
+	private void loadViews() {
+		this.textJokeQuery = this.findViewById(R.id.text_joke_query);
+	}
+
+	private void loadData() {
+		this.textJokeQuery.setText("Some random joke query");
+	}
+
+	public void onJokeButtonClick(View view) {
+		Intent intent = new Intent(this, AnswerActivity.class);
+
+		intent.putExtra(AnswerActivity.JOKE_ANSWER_KEY, "Some random joke answer");
+
+		this.startActivity(intent);
 	}
 }
