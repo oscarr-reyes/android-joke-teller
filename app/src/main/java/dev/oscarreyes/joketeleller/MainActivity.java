0997ae6ee8e035fdf42a2edd4ba33fce44d353e6
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-04-13 21:10:45 -0500

    + Google ads
    * Added google admob dependency
    * Added AdView for showing ads in the application

--- app/src/main/java/dev/oscarreyes/joketeleller/MainActivity.java
+++ app/src/main/java/dev/oscarreyes/joketeleller/MainActivity.java
@@ -1,16 +1,24 @@
 package dev.oscarreyes.joketeleller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import dev.oscarreyes.jokeanswerdisplayer.AnswerActivity;
 
 public class MainActivity extends AppCompatActivity {
+	private static final String TAG = MainActivity.class.getSimpleName();
+
 	private TextView textJokeQuery;
+	private AdView adJoke;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +27,27 @@ public class MainActivity extends AppCompatActivity {
 
 		this.loadViews();
 		this.loadData();
+		this.loadAds();
 	}
 
 	private void loadViews() {
 		this.textJokeQuery = this.findViewById(R.id.text_joke_query);
+		this.adJoke = this.findViewById(R.id.ad_joke);
 	}
 
 	private void loadData() {
 		this.textJokeQuery.setText("Some random joke query");
+	}
+
+	private void loadAds() {
+		// MobileAds.initialize(this, initializationStatus -> {
+		// 	Log.i(TAG, "Ads initialized");
+		// });
+
+		AdRequest adRequest = new AdRequest.Builder()
+			.build();
+
+		this.adJoke.loadAd(adRequest);
 	}
 
 	public void onJokeButtonClick(View view) {
