aad81c447166162071221944061ca215df83fc31
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-04-16 17:36:58 -0500

    * Joke implementation in backend
    * Added `getJoke` api method for the backend which returns a random joke with its query and response

--- backend/src/main/java/dev/oscarreyes/joketeller/backend/MyEndpoint.java
+++ backend/src/main/java/dev/oscarreyes/joketeller/backend/MyEndpoint.java
@@ -4,27 +4,30 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
+import dev.oscarreyes.joke.Joker;
+import dev.oscarreyes.joke.entity.Joke;
 
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ */
 @Api(
-        name = "myApi",
-        version = "v1",
-        namespace = @ApiNamespace(
-                ownerDomain = "backend.builditbigger.gradle.udacity.com",
-                ownerName = "backend.builditbigger.gradle.udacity.com",
-                packagePath = ""
-        )
+	name = "myApi",
+	version = "v1",
+	namespace = @ApiNamespace(
+		ownerDomain = "backend.builditbigger.gradle.udacity.com",
+		ownerName = "backend.builditbigger.gradle.udacity.com",
+		packagePath = ""
+	)
 )
 public class MyEndpoint {
+	@ApiMethod(name = "getJoke")
+	public MyBean getJoke() {
+		Joke joke = Joker.getRandomJoke();
+		MyBean response = new MyBean();
+		String jokeString = String.format("Q: %s; R: %s", joke.getQuery(), joke.getResponse());
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+		response.setData(jokeString);
 
-        return response;
-    }
-
+		return response;
+	}
 }
